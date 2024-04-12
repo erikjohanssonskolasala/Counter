ec0ef753de4610058cb381bce374adf076de629e
Erik Johansson <erik.johansson@skola.sala.se>
2024-04-12 10:57:13 +0200

    Add project files.

---
+++ Averager/Counter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Averager
+{
+    internal class Counter
+    {
+        // TODO lite variabler
+        int value = 0;
+        // ökar räknaren med ett
+        public void Increment()
+        {
+            // TODO
+            value++;
+        }
+
+        // återställer värdet till noll
+        public void Reset()
+        {
+            // TODO
+            value = 0;
+        }
+
+        // ger värdet på räknaren
+        public int GetValue()
+        {
+            // TODO
+            return value;
+        }
+    }
+}
---
+++ Averager/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Averager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Counter counter = new Counter();
+                counter.Increment();
+                counter.Increment();
+                counter.Increment();
+                // ska skriva ut 3
+                Console.WriteLine("counter: " + counter.GetValue());
+                counter.Reset();
+                // ska skriva ut 0
+                Console.WriteLine("counter: " + counter.GetValue());
+        }
+    }
+}
